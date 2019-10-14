6ce438a817dc0c33710480b34518fc53f8345f81
CaroFG <carolina.ferreira131@gmail.com>
2019-10-14 18:35:32 +0200

    seed items ok, man woman sections ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,9 @@ Type.destroy_all
 @pull = Type.create(name: "pull")
 @chemise = Type.create(name: "chemise")
 @pantalon = Type.create(name: "pantalon")
+@totallook = Type.create(name: "totallook")
+@robe = Type.create(name: "robe")
+@veste = Type.create(name: "veste")
 
 # ---------------------------- We create our users and our items ---------------------------- #
 20.times do 
@@ -28,27 +31,75 @@ Type.destroy_all
 		first_name: Faker::Name.first_name,
 		last_name: Faker::Name.last_name,
 		password: Faker::Internet.password)
-
-	item = Item.create!(
-		name: "homme",
-		price: rand(1..1000),
-		category: @male,
-		type: @pantalon)
-	
-	item = Item.create!(
-		name: "femme",
-		price: rand(1..1000),
-		category: @female,
-		type: @chemise)
-
-
 	# Puts to show every time an item is added
 	puts "Item added"
 end
 
 
 
+item = Item.create!(
+		name: "Total look",
+		price: 70,
+		category: @female,
+		type: @totallook,
+		description: Faker::Lorem.sentence,
+		image_path: "sangoku-woman.jpg")
 
+item = Item.create!(
+		name: "Chemisier en satin",
+		price: 40,
+		category: @female,
+		type: @chemise,
+		description: Faker::Lorem.sentence,
+		image_path: "plant-girl.jpg")
+
+item = Item.create!(
+	name: "Total look bleu",
+	price: 70,
+	category: @female,
+	type: @totallook,
+	description: Faker::Lorem.sentence,
+	image_path: "jean.jpg")
+
+item = Item.create!(
+	name: "Robe à motifs",
+	price: 35,
+	category: @female,
+	type: @robe,
+	description: Faker::Lorem.sentence,
+	image_path: "dress.jpg")
+
+item = Item.create!(
+	name: "Veste en daim",
+	price: 100,
+	category: @male,
+	type: @veste,
+	description: Faker::Lorem.sentence,
+	image_path: "man1.jpeg")
+
+item = Item.create!(
+	name: "Pantalon extra-slim",
+	price: 45,
+	category: @male,
+	type: @pantalon,
+	description: Faker::Lorem.sentence,
+	image_path: "trouser-man.jpg")
+
+item = Item.create!(
+	name: "Pantalon à carreaux",
+	price: 40,
+	category: @male,
+	type: @pantalon,
+	description: Faker::Lorem.sentence,
+	image_path: "check-pants.jpg")
+
+item = Item.create!(
+	name: "Veste de costume",
+	price: 55,
+	category: @male,
+	type: @veste,
+	description: Faker::Lorem.sentence,
+	image_path: "charles.jpg")
 
 # We create an admin
 user = User.create!(

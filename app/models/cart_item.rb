2b4d564e3baa8270c81589f00fd47559f4bbcae1
CaroFG <carolina.ferreira131@gmail.com>
2019-10-24 11:28:28 +0200

    sizes view to be perfected

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,7 @@
 class CartItem < ApplicationRecord
 	belongs_to :cart
 	belongs_to :item_size
+
+
+
 end
---
+++ test/controllers/item_sizes_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ItemSizesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

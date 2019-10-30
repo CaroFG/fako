6396a6fee91a38ef4b82b27035a58413b6b8c961
CaroFG <carolina.ferreira131@gmail.com>
2019-10-30 16:16:29 +0100

    search improved including item description

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,7 +3,7 @@ class SearchesController < ApplicationController
 		@items = Item.all 
 		@search = params[:search]
 	    if @search.present?
-	      @items = Item.where("name ILIKE ?", "%#{@search}%")
+	      @items = Item.where("name ILIKE ?", "%#{@search}%").or(Item.where("description ILIKE ?", "%#{@search}%"))
 	    end
 	end
 end

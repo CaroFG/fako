fb00a7ed2f1945fe1ff525ba2399526e951abd00
CaroFG <carolina.ferreira131@gmail.com>
2019-11-10 20:55:31 +0100

    form tag for new item created

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -6,6 +6,21 @@ class Admins::ItemsController < ApplicationController
 		@items = Item.all
 	end
 
+	def new
+		@item = Item.new
+	end
+
+	def create
+		@item = Item.new(name: params[:name], price: params[:price], description: params[:description], category: params[:category], type: params[:type])
+			if @item.save
+				redirect_to admins_users_path 
+				flash[:success] = "L'article a bien été crée"
+			else 
+				flash[:alert] = "Un problème est survenu, veuillez reessayer"
+				render :new
+			end
+	end
+
 	def check_if_admin
 		if current_user.is_admin == false
 			flash[:error] = "Vous n'êtes pas un administrateur !"
